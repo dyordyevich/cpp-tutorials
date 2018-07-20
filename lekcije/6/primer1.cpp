aadb0b826f2a6cff360b3b3c2e6bca0d6f49d663
nik994 <nik994pa@yandex.ru>
2018-07-20 14:40:47 +0200

    Dodate su datoteke za 6. lekciju.

---
+++ lekcije/6/primer1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+int a,b;
+a=5;
+b=10;
+
+cout<<(a>b)<<endl;
+
+system("pause");
+return 0;
+}
---
+++ lekcije/6/primer2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+bool x,y;
+x=true;
+y=false;
+
+cout<<(x&&(!y))<<endl;
+
+system("pause");
+return 0;
+}
