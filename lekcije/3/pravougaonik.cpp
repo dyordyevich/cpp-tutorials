8326f9c3dbfd651c513b385d35ae814dba9a44f2
nik994 <nik994pa@yandex.ru>
2018-07-20 14:03:31 +0200

    Dodata je datoteka za trecu lekciju

---
+++ lekcije/3/pravougaonik.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main(){
+//Varijabla a
+int a;
+//Varijabla b
+int b;
+/*Varijabla 
+P*/
+int P;
+//Varijabla O
+int O;
+
+cout<<"Unesite vrednost za a:";
+cin>>a;
+
+cout<<"Unesite vrednost za b:";
+cin>>b;
+
+//Obim
+O=2*a+2*b;
+//Povrsina
+P=a*b;
+
+cout<<"Povrsina pravougaonika je "<<P<<endl;
+
+cout<<"Obim pravougaonika je ";
+cout<<O<<endl;
+
+/*Kraj programa
+C++ ucenje*/
+
+system("pause");
+return 0;    
+}
