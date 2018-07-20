f856dd39c622c4d9f3452b7618871c4176aaf933
nik994 <nik994pa@yandex.ru>
2018-07-20 13:52:14 +0200

    Dodate su datoteke za 2. lekciju

---
+++ lekcije/2/drugi_program.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"C++ ucenje"<<endl;
+cout<<"Nikola Djordjevic\n";
+system("pause");
+return 0;    
+}
---
+++ lekcije/2/prvi_program.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+cout<<"Moj prvi program\n";
+system("pause");
+return 0;    
+}
